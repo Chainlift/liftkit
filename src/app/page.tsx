8b4ef49de8b163cb1c3653d798286c4de7a7f459
radodd <ethanf.flores@gmail.com>
2025-04-30 19:59:29 -0400

    feat(button): add dynamic styles for variant, color, size props
    chore: prettier formatting changes in liftkit-core.css
    
    chore (button component): reset branch to 2395bc7 - reverted to previous commit to discard post-rebase changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,12 @@ import Text from "@/liftkit/components/text";
 import Row from "@/liftkit/components/row";
 import styles from "./page.module.css";
 import Sticker from "@/liftkit/components/sticker";
+import Button from "@/liftkit/components/button";
 
 export default function Home() {
+  const variants = ["fill", "outline", "text"] as const;
+  const sizes = ["sm", "md", "lg"] as const;
+
   return (
     <div className={styles.page}>
       <Text fontClass="display1" tag="footer" color="primary">
@@ -59,6 +63,7 @@ export default function Home() {
             <div style={{ background: "#aaf", padding: "1rem" }}>Grow 3</div>
           </Row>
         </div>
+
         <div style={{ display: "flex", flexWrap: "wrap", gap: "1rem" }}>
           <Sticker content="Default" />
           <Sticker content="Primary" color="primary" />
@@ -74,6 +79,61 @@ export default function Home() {
           <Sticker content="Surface Variant" color="surfacevariant" />
         </div>
       </div>
+
+      {variants.map((variant) => (
+        <div key={variant}>
+          <h2>{variant} Variant</h2>
+          <div>
+            {sizes.map((size) => (
+              <div key={`${variant}-${size}`}>
+                <h3>Size: {size}</h3>
+                <div>
+                  <div>
+                    <Button
+                      label="Left Icon"
+                      variant={variant}
+                      size={size}
+                      color="error"
+                      startIcon="airplay"
+                    />
+                    <span>startIcon</span>
+                  </div>
+                  <div>
+                    <Button
+                      label="Right Icon"
+                      variant={variant}
+                      size={size}
+                      color="primary"
+                      endIcon="airplay"
+                    />
+                    <span>endIcon</span>
+                  </div>
+                  <div>
+                    <Button
+                      label="Both Icons"
+                      variant={variant}
+                      size={size}
+                      color="primary"
+                      startIcon="airplay"
+                      endIcon="airplay"
+                    />
+                    <span>both</span>
+                  </div>
+                  <div>
+                    <Button
+                      label="No Icon"
+                      variant={variant}
+                      size={size}
+                      color="primary"
+                    />
+                    <span>none</span>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
