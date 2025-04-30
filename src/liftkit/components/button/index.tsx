0de294e0b14506119b6ec5ba42520026e90af3a6
radodd <ethanf.flores@gmail.com>
2025-04-30 19:59:29 -0400

    feat(button): add dynamic styles for variant, color, size props

--- src/liftkit/components/button/index.tsx
+++ src/liftkit/components/button/index.tsx
@@ -12,24 +12,39 @@ interface LkButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 }
 
 export default function Button(props: LkButtonProps) {
-  const { label, startIcon, endIcon, ...restProps } = props;
+  const { label, startIcon, endIcon, color, size, variant, material, ...restProps } = props;
 
-  const lkButtonAttrs = useMemo(
-    () => propsToDataAttrs(restProps, "lk-button"),
-    [
-      // restProps.variant,
-      // restProps.color,
-      // restProps.size,
-      // restProps.material,
-      // startIcon,
-      // endIcon,
-      restProps,
-    ],
-  );
+  const lkButtonAttrs = useMemo(() => {
+    return {
+      ...propsToDataAttrs(restProps, "lk-button"),
+      ...(size && { "data-lk-button-size": size }),
+      ...(color && {"data-lk-button-color": color}),
+      ...(variant && {"data-lk-button-variant": variant}),
+      ...(material && {"data-lk-button-variant": material})
+    };
+  }, [restProps]);
 
   return (
-    <button {...lkButtonAttrs} type="button">
-      <div lk-button-content-wrap>
+    <button
+      {...lkButtonAttrs}
+      type="button"
+      data-lk-component="button"
+      style={
+        color && variant === "fill"
+          ? {
+              backgroundColor: `var(--light__${color}_lkv)`,
+              color: `var(--light__on${color}_lkv)`,
+            }
+          : color && variant === "outline"
+          ? {
+              borderColor: color === "inversesurface" ? `var(--light__inversesurface_lk)` : `var(--lk-outline)`,
+              color: `var(--light__${color}_lkv)`,
+              backgroundColor: "transparent",
+            }
+          : undefined
+      }
+    >
+      <div lk-button-content-wrap="true">
         {startIcon && (
           <i lk-component="icon" lk-icon-position="start">
             {startIcon}
