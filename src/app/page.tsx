91f23e72584000afd251e9fa2759af7e2b2c62d9
radodd <ethanf.flores@gmail.com>
2025-04-30 21:49:21 -0400

    feat(badge): add dynamic styles for color, scale, iconStrokeWidth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import Badge from "@/liftkit/components/badge";
 import styles from "./page.module.css";
 import Welcome from "@/content/components/button.mdx";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <Welcome />
+      <Badge icon="replace" color="surfacecontainerhigh" scale="lg" iconStrokeWidth={1}/>
+      <span className="absolute top-0">Hello</span>
+      {/* <Welcome /> */}
     </div>
   );
 }
