cc85d85e6b7dcca148bcafc25769b14761d25230
Ethan <96605781+radodd@users.noreply.github.com>
2025-04-30 22:05:48 -0400

    chore: cleaned up Welcome page
    I viewed the badge component here and simply returned this welcome page to its original state.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
-import Badge from "@/liftkit/components/badge";
 import styles from "./page.module.css";
 import Welcome from "@/content/components/button.mdx";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <Badge icon="replace" color="surfacecontainerhigh" scale="lg" iconStrokeWidth={1}/>
-      <span className="absolute top-0">Hello</span>
-      {/* <Welcome /> */}
+      <Welcome />
     </div>
   );
 }
