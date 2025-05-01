30494bfe570ff80ef2a2f31b197c9d3fe0cf13e5
radodd <ethanf.flores@gmail.com>
2025-04-30 20:00:34 -0400

    chore: prettier formatting changes in liftkit-core.css
    chore (button component): reset branch to 2395bc7 - reverted to previous commit to discard post-rebase changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,73 @@
-import styles from "./page.module.css";
-import Welcome from "@/content/components/button.mdx";
+
+import Button from "@/liftkit/components/button";
+
+
 
 export default function Home() {
+  const variants = ["fill", "outline", "text"] as const;
+  const sizes = ["sm", "md", "lg"] as const;
+
   return (
-    <div className={styles.page}>
-      <Welcome />
+
+<div>
+      {variants.map((variant) => (
+        <div key={variant}>
+          <h2>{variant} Variant</h2>
+          <div>
+            {sizes.map((size) => (
+              <div key={`${variant}-${size}`}>
+                <h3>Size: {size}</h3>
+                <div>
+                  <div>
+                    <Button
+                      label="Left Icon"
+                      variant={variant}
+                      size={size}
+                      color="error"
+                      startIcon="airplay"
+                    />
+                    <span>startIcon</span>
+                  </div>
+                  <div>
+                    <Button
+                      label="Right Icon"
+                      variant={variant}
+                      size={size}
+                      color="primary"
+                      endIcon="airplay"
+                    />
+                    <span>endIcon</span>
+                  </div>
+                  <div>
+                    <Button
+                      label="Both Icons"
+                      variant={variant}
+                      size={size}
+                      color="primary"
+                      startIcon="airplay"
+                      endIcon="airplay"
+                    />
+                    <span>both</span>
+                  </div>
+                  <div>
+                    <Button
+                      label="No Icon"
+                      variant={variant}
+                      size={size}
+                      color="primary"
+                    />
+                    <span>none</span>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
+
+    
+
     </div>
   );
+ 
 }
