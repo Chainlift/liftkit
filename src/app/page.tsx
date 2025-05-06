8a43cf48eb25625d610252c32f86c216f1d235c2
radodd <ethanf.flores@gmail.com>
2025-05-06 13:43:49 -0400

    replaced img with Image component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -384,10 +384,9 @@ export default function Home() {
       <div className="size-test-grid">
         {sizes.map((size) => (
           <figure key={size} className="size-test-item">
-            <img
+            <Image
               src="/testimage.png"
               alt={size}
-              lk-component="image"
               lk-image-width={size}
               lk-image-height={size}
               className="size-test-img"
@@ -401,10 +400,9 @@ export default function Home() {
       <div className="radius-test-grid">
         {radii.map((radius) => (
           <figure key={radius} className="radius-test-item">
-            <img
+            <Image
               src="/testimage.png"
               alt={radius}
-              lk-component="image"
               lk-image-border-radius={radius}
               className="radius-test-img"
             />
@@ -449,10 +447,9 @@ export default function Home() {
       <div className="aspect-test-grid">
         {aspectRatios.map((ratio) => (
           <figure key={ratio} style={{ border: "1px solid #ccc" }}>
-            <img
+            <Image
               src="/testimage.png"
               alt={ratio}
-              lk-component="image"
               lk-image-aspect={ratio}
               style={{ width: "100%", objectFit: "cover" }}
             />
