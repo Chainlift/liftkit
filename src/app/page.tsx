fdd40e5bf0bc08b0baaa5001f1b9e5c2dd5606a0
radodd <ethanf.flores@gmail.com>
2025-05-05 20:58:41 -0400

    feat: sticker component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,24 @@
-import Icon from "@/liftkit/components/icon";
-import Badge from "@/liftkit/components/badge";
+
 import styles from "./page.module.css";
+import Sticker from "@/liftkit/components/sticker";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <Icon name="airplay" color="primary" fontClass="title2"/>
-      <Badge></Badge>
+  <div style={{ display: "flex", flexWrap: "wrap", gap: "1rem" }}>
+      <Sticker content="Default" />
+      <Sticker content="Primary" color="primary" />
+      <Sticker content="Secondary" color="secondary" />
+      <Sticker content="Tertiary" color="tertiary" />
+      <Sticker content="Error" color="error" />
+      <Sticker content="Warning" color="warning" />
+      <Sticker content="Success" color="success" />
+      <Sticker content="Info" color="info" />
+      <Sticker content="Surface" color="surface" />
+      <Sticker content="Inverse" color="inversesurface" />
+      <Sticker content="Primary Container" color="primarycontainer" />
+      <Sticker content="Surface Variant" color="surfacevariant" />
+    </div>
     </div>
   );
 }
