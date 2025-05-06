f1a040bbbb846bb1442d3397a5c829b9842e4da1
radodd <ethanf.flores@gmail.com>
2025-05-05 22:09:37 -0400

    feat: text component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
-import Icon from "@/liftkit/components/icon";
-import Badge from "@/liftkit/components/badge";
-import styles from "./page.module.css";
+import Text from "@/liftkit/components/text";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <Icon name="airplay" color="primary" fontClass="title2"/>
-      <Badge></Badge>
+    <div>
+      <Text fontClass="display1" tag="footer" color="primary">
+        Hello World
+      </Text>
     </div>
   );
 }
