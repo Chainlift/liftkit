adc2e85dc868f66491cc39d375db8491fbc720cc
Garrett Mack <garrett@chainlift.io>
2025-06-11 12:04:12 -0700

    set up material layer interface and types
    related to #138
    
    - define type template for setting unique props for each type of material. For example: "LkMatProps_Glass"
    - define base markup and prop passing

--- registry/nextjs/components/material-layer/index.tsx
+++ registry/nextjs/components/material-layer/index.tsx
@@ -1,4 +1,22 @@
-export default function MaterialLayer() {
+import "@/registry/nextjs/material-layer/material-layer.css";
 
+type LkMatProps_Glass = {
+  thickness?: "thick" | "normal" | "thin"; // Thickness of the glass material. Thicker material blurs more.
+};
 
+type LkMatProps_Flat = {};
+
+interface LkMaterialLayerProps extends React.HTMLAttributes<HTMLDivElement> {
+  zIndex?: number; // Optional z-index for the material layer. Different use cases might need it to be at different z-indexes.
+  material?: "glass" | "debug";
+  materialSpecs?: LkMatProps_Flat | LkMatProps_Glass; // Optional material-specific properties
+}
+
+export default function MaterialLayer({ zIndex = 0, material, materialSpecs }: LkMaterialLayerProps) {
+
+  return (
+    <>
+      <div lk-component="material-layer" lk-material-type={material} style={{zIndex: zIndex}}></div>
+    </>
+  );
 }
