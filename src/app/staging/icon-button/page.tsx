14fd14163b07168c371e0954793cf611114469dc
Garrett Mack <garrett@chainlift.io>
2025-06-12 10:07:30 -0700

    feat: add icon-button staging area

---
+++ src/app/staging/icon-button/page.tsx
@@ -0,0 +1,14 @@
+import Container from "@/registry/nextjs/components/containers";
+import Heading from "@/registry/nextjs/components/heading";
+import Section from "@/registry/nextjs/components/section";
+
+export default function IconButtonStaging() {
+return <>
+    <Section>
+        <Container>
+            <Heading tag="h1" className="display2-bold m-bottom-md">Icon Button Staging</Heading>
+        </Container>
+    </Section>
+</>
+
+}
