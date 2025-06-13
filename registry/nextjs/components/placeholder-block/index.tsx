eec4776a95010c586d2f39ae600b647032fb4e71
Garrett Mack <garrett@chainlift.io>
2025-06-13 08:19:07 -0700

    switch to regular css instead of styled-jsx

--- registry/nextjs/components/placeholder-block/index.tsx
+++ registry/nextjs/components/placeholder-block/index.tsx
@@ -1,17 +1,12 @@
+import "@/registry/nextjs/components/placeholder-block/placeholder-block.css";
+
 export default function PlaceholderBlock() {
   return (
     <>
       <div lk-component="placeholder-block">
         <div className="capline-mono opacity-60">Placeholder</div>
       </div>
-      <style jsx>{`
-        [lk-component="placeholder-block"] {
-          display: block;
-          border: 1px dashed var(--lk-color-info);
-          background-color: var(--lk-color-infocontainer);
-          padding: var(--lk-size-md);
-        }
-      `}</style>
+
     </>
   );
 }
