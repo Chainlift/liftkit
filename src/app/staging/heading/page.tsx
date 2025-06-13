99185135f9ebeca85c209ff3f8e50eb0c5b3a15b
Garrett Mack <garrett@chainlift.io>
2025-06-13 16:08:40 -0700

    patch: section prop passing
    wasn't spreading restProps correctly and it was interfering with my heading tests

--- src/app/staging/heading/page.tsx
+++ src/app/staging/heading/page.tsx
@@ -1,68 +1,69 @@
 import Column from "@/registry/nextjs/components/column";
 import Heading from "@/registry/nextjs/components/heading";
-
+import Row from "@/registry/nextjs/components/row";
+import Section from "@/registry/nextjs/components/section";
 
 const LkColors: LkColor[] = [
-    "primary",
-    "onprimary",
-    "primarycontainer",
-    "onprimarycontainer",
-    "secondary",
-    "onsecondary",
-    "secondarycontainer",
-    "onsecondarycontainer",
-    "tertiary",
-    "ontertiary",
-    "tertiarycontainer",
-    "ontertiarycontainer",
-    "error",
-    "onerror",
-    "errorcontainer",
-    "onerrorcontainer",
-    "background",
-    "onbackground",
-    "surface",
-    "onsurface",
-    "surfacevariant",
-    "onsurfacevariant",
-    "shadow",
-    "inversesurface",
-    "scrim",
-    "inverseonsurface",
-    "inverseprimary",
-    "success",
-    "onsuccess",
-    "successcontainer",
-    "onsuccesscontainer",
-    "warning",
-    "onwarning",
-    "warningcontainer",
-    "onwarningcontainer",
-    "info",
-    "oninfo",
-    "infocontainer",
-    "oninfocontainer",
-    "primaryfixed",
-    "onprimaryfixed",
-    "primaryfixeddim",
-    "onprimaryfixedvariant",
-    "secondaryfixed",
-    "onsecondaryfixed",
-    "secondaryfixeddim",
-    "onsecondaryfixedvariant",
-    "tertiaryfixed",
-    "ontertiaryfixed",
-    "tertiaryfixeddim",
-    "ontertiaryfixedvariant",
-    "surfacedim",
-    "surfacebright",
-    "surfacecontainerlowest",
-    "surfacecontainerlow",
-    "surfacecontainer",
-    "surfacecontainerhigh",
-    "surfacecontainerhighest",
-    "outline",
-    "outlinevariant"
+  "primary",
+  "onprimary",
+  "primarycontainer",
+  "onprimarycontainer",
+  "secondary",
+  "onsecondary",
+  "secondarycontainer",
+  "onsecondarycontainer",
+  "tertiary",
+  "ontertiary",
+  "tertiarycontainer",
+  "ontertiarycontainer",
+  "error",
+  "onerror",
+  "errorcontainer",
+  "onerrorcontainer",
+  "background",
+  "onbackground",
+  "surface",
+  "onsurface",
+  "surfacevariant",
+  "onsurfacevariant",
+  "shadow",
+  "inversesurface",
+  "scrim",
+  "inverseonsurface",
+  "inverseprimary",
+  "success",
+  "onsuccess",
+  "successcontainer",
+  "onsuccesscontainer",
+  "warning",
+  "onwarning",
+  "warningcontainer",
+  "onwarningcontainer",
+  "info",
+  "oninfo",
+  "infocontainer",
+  "oninfocontainer",
+  "primaryfixed",
+  "onprimaryfixed",
+  "primaryfixeddim",
+  "onprimaryfixedvariant",
+  "secondaryfixed",
+  "onsecondaryfixed",
+  "secondaryfixeddim",
+  "onsecondaryfixedvariant",
+  "tertiaryfixed",
+  "ontertiaryfixed",
+  "tertiaryfixeddim",
+  "ontertiaryfixedvariant",
+  "surfacedim",
+  "surfacebright",
+  "surfacecontainerlowest",
+  "surfacecontainerlow",
+  "surfacecontainer",
+  "surfacecontainerhigh",
+  "surfacecontainerhighest",
+  "outline",
+  "outlinevariant",
 ];
 
 export default function HeadingStaging() {
@@ -81,23 +82,25 @@ export default function HeadingStaging() {
     "capline",
   ];
 
-
-
-
-
-  return <>
-  
-    {fontClasses.map((fontClass) => <Column key={fontClass} gap="md">{getHeadingList(fontClass)}</Column>)}
-  
-  </>;
+  return (
+    <>
+      <Section padding="md" style={{maxWidth: "100vw", overflowX: "auto"}}>
+        <Row gap="md">
+          {fontClasses.map((fontClass) => (
+            <Column key={fontClass} gap="md">
+              {getHeadingList(fontClass)}
+            </Column>
+          ))}
+        </Row>
+      </Section>
+    </>
+  );
 }
 
 function getHeadingList(fontClass: LkFontClass) {
-
   const headingTags: LkHeadingTag[] = ["h1", "h2", "h3", "h4", "h5", "h6"];
   return (
     <>
-
       {headingTags.map((tag) => (
         <Heading key={tag} tag={tag} fontClass={fontClass} fontColor="primary">
           Heading
