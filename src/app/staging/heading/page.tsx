999d2cc6c65434370f43f11394a4806f26532300
Garrett Mack <garrett@chainlift.io>
2025-06-13 16:05:52 -0700

    render all heading tags in all font scales

--- src/app/staging/heading/page.tsx
+++ src/app/staging/heading/page.tsx
@@ -1,6 +1,108 @@
+import Column from "@/registry/nextjs/components/column";
+import Heading from "@/registry/nextjs/components/heading";
+
+
+const LkColors: LkColor[] = [
+    "primary",
+    "onprimary",
+    "primarycontainer",
+    "onprimarycontainer",
+    "secondary",
+    "onsecondary",
+    "secondarycontainer",
+    "onsecondarycontainer",
+    "tertiary",
+    "ontertiary",
+    "tertiarycontainer",
+    "ontertiarycontainer",
+    "error",
+    "onerror",
+    "errorcontainer",
+    "onerrorcontainer",
+    "background",
+    "onbackground",
+    "surface",
+    "onsurface",
+    "surfacevariant",
+    "onsurfacevariant",
+    "shadow",
+    "inversesurface",
+    "scrim",
+    "inverseonsurface",
+    "inverseprimary",
+    "success",
+    "onsuccess",
+    "successcontainer",
+    "onsuccesscontainer",
+    "warning",
+    "onwarning",
+    "warningcontainer",
+    "onwarningcontainer",
+    "info",
+    "oninfo",
+    "infocontainer",
+    "oninfocontainer",
+    "primaryfixed",
+    "onprimaryfixed",
+    "primaryfixeddim",
+    "onprimaryfixedvariant",
+    "secondaryfixed",
+    "onsecondaryfixed",
+    "secondaryfixeddim",
+    "onsecondaryfixedvariant",
+    "tertiaryfixed",
+    "ontertiaryfixed",
+    "tertiaryfixeddim",
+    "ontertiaryfixedvariant",
+    "surfacedim",
+    "surfacebright",
+    "surfacecontainerlowest",
+    "surfacecontainerlow",
+    "surfacecontainer",
+    "surfacecontainerhigh",
+    "surfacecontainerhighest",
+    "outline",
+    "outlinevariant"
+];
+
 export default function HeadingStaging() {
+  const fontClasses: LkFontClass[] = [
+    "display1",
+    "display2",
+    "title1",
+    "title2",
+    "title3",
+    "heading",
+    "subheading",
+    "body",
+    "callout",
+    "label",
+    "caption",
+    "capline",
+  ];
 
 
 
 
+
+  return <>
+  
+    {fontClasses.map((fontClass) => <Column key={fontClass} gap="md">{getHeadingList(fontClass)}</Column>)}
+  
+  </>;
+}
+
+function getHeadingList(fontClass: LkFontClass) {
+
+  const headingTags: LkHeadingTag[] = ["h1", "h2", "h3", "h4", "h5", "h6"];
+  return (
+    <>
+
+      {headingTags.map((tag) => (
+        <Heading key={tag} tag={tag} fontClass={fontClass} fontColor="primary">
+          Heading
+        </Heading>
+      ))}
+    </>
+  );
 }
