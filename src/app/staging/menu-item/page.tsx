2dd6b554f6f4ee2663c88375730503c6d0f114a4
Garrett Mack <garrett@chainlift.io>
2025-06-14 08:49:54 -0700

    create staging

---
+++ src/app/staging/menu-item/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+import Column from "@/registry/nextjs/components/column";
+import Heading from "@/registry/nextjs/components/heading";
+import Row from "@/registry/nextjs/components/row";
+import Section from "@/registry/nextjs/components/section";
+import Image from "@/registry/nextjs/components/image";
+import Container from "@/registry/nextjs/components/containers";
+import Grid from "@/registry/nextjs/components/grid";
+import MenuItem from "@/registry/nextjs/components/menu-item";
+
+export default function MenuItemStaging() {
+  return (
+    <>
+      <Section>
+        <Heading tag="h2">Image Component Tests</Heading>
+        <MenuItem></MenuItem>
+      </Section>
+    </>
+  );
+}
