7280d46ecfd2ab0586ebc40b784fca032e7f8aae
Garrett Mack <garrett@chainlift.io>
2025-06-15 11:16:02 -0700

    set up staging

---
+++ src/app/staging/sticker/page.tsx
@@ -0,0 +1,12 @@
+import Column from "@/registry/nextjs/components/column";
+import Sticker from "@/registry/nextjs/components/sticker";
+
+export default function StickerStaging() {
+  return (
+    <>
+      <Column gap="md">
+        <Sticker>Sticker</Sticker>
+      </Column>
+    </>
+  );
+}
