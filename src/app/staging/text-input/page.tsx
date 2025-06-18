1f21776423ad23f64035ee7b89e1b9479744b348
Garrett Mack <garrett@chainlift.io>
2025-06-17 17:08:15 -0700

    lint: remove unused imports

--- src/app/staging/text-input/page.tsx
+++ src/app/staging/text-input/page.tsx
@@ -1,12 +1,7 @@
 "use client";
-import Grid from "@/registry/nextjs/components/grid";
 import Container from "@/registry/nextjs/components/containers";
 import Section from "@/registry/nextjs/components/section";
-import Sticker from "@/registry/nextjs/components/sticker";
-import { LkColors } from "@/registry/universal/lib/utils/debugUtils";
-import Tabs from "@/registry/nextjs/components/tabs";
 import Column from "@/registry/nextjs/components/column";
-import Row from "@/registry/nextjs/components/row";
 import TextInput from "@/registry/nextjs/components/text-input";
 
 export default function TextInputStaging() {
