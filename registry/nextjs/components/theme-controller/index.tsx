11ed780f7ba87fa57881d75c7dbcf3438dcf1262
Garrett Mack <garrett@chainlift.io>
2025-07-06 20:22:54 -0700

    polish the pre block

--- registry/nextjs/components/theme-controller/index.tsx
+++ registry/nextjs/components/theme-controller/index.tsx
@@ -78,50 +78,61 @@ export default function ThemeController() {
 
   const [isOpen, setIsOpen] = useState(false);
 
-  if (typeof document !== "undefined") {
-    return (
-      <>
-        {!isOpen && (
-          <div className="position-fixed" style={{ zIndex: 1000 }}>
-            <IconButton
-              icon="palette"
-              fontClass="display2"
-              onClick={() => setIsOpen(true)}
-              className="shadow-xl"
-              color="inversesurface"
-              style={{
-                position: "fixed",
-                zIndex: 1000,
-                top: "var(--lk-size-xs)",
-                left: "var(--lk-size-xs)",
-              }}
-            />
-          </div>
-        )}
-        {isOpen &&
-          createPortal(
-            <div className="lk-theme-drawer">
-              <Card
-                scaleFactor="heading"
-                bgColor="surfacecontainerlowest"
-                className="shadow-lg m-bottom-2xl h-full"
-                material="glass"
-                materialProps={{ thickness: "normal" }}
-                isScrollable={true}
-              >
-                <Column gap="md">
-                  <Row alignItems="center" justifyContent="space-between">
-                    <h2 className="body-bold">Theme Controller</h2>
-                    <IconButton
-                      icon="x"
-                      variant="outline"
-                      color="onsurface"
-                      onClick={() => setIsOpen(false)}
-                    ></IconButton>
-                  </Row>
+  return (
+    <>
+      {!isOpen && (
+        <div className="position-fixed" style={{ zIndex: 1000 }}>
+          <IconButton
+            icon="palette"
+            fontClass="display2"
+            onClick={() => setIsOpen(true)}
+            className="shadow-xl"
+            color="inversesurface"
+            style={{
+              position: "fixed",
+              zIndex: 1000,
+              top: "var(--lk-size-xs)",
+              left: "var(--lk-size-xs)",
+            }}
+          />
+        </div>
+      )}
+      {isOpen &&
+        createPortal(
+          <div className="lk-theme-drawer">
+            <Card
+              scaleFactor="heading"
+              bgColor="surfacecontainerlowest"
+              className="shadow-lg m-bottom-2xl h-full"
+              material="glass"
+              materialProps={{ thickness: "normal" }}
+              isScrollable={true}
+            >
+              <Column gap="md">
+                <Row alignItems="center" justifyContent="space-between">
+                  <h2 className="body-bold">Theme Controller</h2>
 
-                  <Card bgColor="surfacecontainerlowest" scaleFactor="body" className="position-relative">
-                    <pre className="caption">
+                  <IconButton
+                    icon="x"
+                    variant="outline"
+                    color="onsurface"
+                    onClick={() => setIsOpen(false)}
+                  ></IconButton>
+                </Row>
+                <div>
+                  <h2 className="label m-bottom-xs">Config</h2>
+                  <p className="caption color-onsurfacevariant m-bottom-xs">
+                    Copy and paste this snippet into your theme/index.tsx file to update your project to match the current configuration.
+                  </p>
+
+                  <Card
+                    material="glass"
+                    materialProps={{ thickness: "thin", tint: "onsurface", tintOpacity: 0.1 }}
+                    bgColor="surfacecontainerlowest"
+                    scaleFactor="body"
+                    className="position-relative"
+                  >
+                    <pre style={{ fontSize: "0.618em", overflow: "auto" }}>
                       {`
 const [colorMode, setColorMode] = useState<"light" | "dark">("${colorMode}");
 
@@ -134,176 +145,176 @@ const [palette, setPalette] = useState<PaletteState>(${JSON.stringify(palette, n
                       onClick={handleCopyPalette}
                     ></IconButton>
                   </Card>
+                </div>
+                <div>
+                  <h2 className="capline m-bottom-lg color-onsurfacevariant">Mode</h2>
+                  <Row alignItems="start" gap="md">
+                    <Column>
+                      <Switch onClick={handleColorModeSwitch} value={colorMode === "dark" ? true : false}></Switch>
+                    </Column>
+                    <Column>
+                      <label className="label m-bottom-xs">Default to Dark Mode</label>
+                      <p className="caption color-onsurfacevariant m-bottom-xs">Toggles dark mode.</p>
+                    </Column>
+                  </Row>
+                </div>
+                <div>
+                  <h2 className="capline m-bottom-lg color-onsurfacevariant">Globals</h2>
+                  <Row alignItems="start" gap="md">
+                    <input
+                      type="color"
+                      name="master"
+                      value={palette["master"]}
+                      onChange={(event) => handleColorChange("master", event.target.value)}
+                    ></input>
+                    <Column>
+                      <label className="label m-bottom-xs" htmlFor={"master"}>
+                        {"master"}
+                      </label>
+                      <p className="caption color-onsurfacevariant m-bottom-xs">
+                        The seed color.{" "}
+                        <strong className="color-error">If you edit this, all other color tokens will reset.</strong>
+                      </p>
+                    </Column>
+                  </Row>
+                </div>
+                <div>
                   <div>
-                    <h2 className="capline m-bottom-lg">Mode</h2>
-                    <Row alignItems="start" gap="md">
-                      <Column>
-                        <Switch onClick={handleColorModeSwitch} value={colorMode === "dark" ? true : false}></Switch>
-                      </Column>
-                      <Column>
-                        <label className="label m-bottom-xs">Default to Dark Mode</label>
-                        <p className="caption color-onsurfacevariant m-bottom-xs">Toggles dark mode.</p>
-                      </Column>
-                    </Row>
+                    <h2 className="capline m-bottom-lg color-onsurfacevariant">Brand</h2>
                   </div>
-                  <div>
-                    <h2 className="capline m-bottom-lg">Globals</h2>
-                    <Row alignItems="start" gap="md">
+                  {brandPalette.map((colorGroup) => (
+                    <Row key={colorGroup} alignItems="start" gap="md" className="m-bottom-sm">
                       <input
                         type="color"
-                        name="master"
-                        value={palette["master"]}
-                        onChange={(event) => handleColorChange("master", event.target.value)}
+                        name={colorGroup}
+                        value={palette[colorGroup]}
+                        onChange={(event) => handleColorChange(colorGroup, event.target.value)}
                       ></input>
                       <Column>
-                        <label className="label m-bottom-xs" htmlFor={"master"}>
-                          {"master"}
+                        <label className="caption-bold mono m-bottom-2xs" htmlFor={colorGroup}>
+                          {colorGroup}
                         </label>
                         <p className="caption color-onsurfacevariant m-bottom-xs">
-                          The seed color.{" "}
-                          <strong className="color-error">If you edit this, all other color tokens will reset.</strong>
+                          {colorGroup === "primary"
+                            ? "Main brand color, used for most UI elements."
+                            : colorGroup === "secondary"
+                              ? "Desaturated variant of primary."
+                              : colorGroup === "tertiary"
+                                ? "Your accent color. Defaults to complementary hue to primary."
+                                : null}
                         </p>
                       </Column>
                     </Row>
-                  </div>
+                  ))}
+                </div>
+                <div>
                   <div>
-                    <div>
-                      <h2 className="capline m-bottom-lg color-onsurfacevariant">Brand</h2>
-                    </div>
-                    {brandPalette.map((colorGroup) => (
-                      <Row key={colorGroup} alignItems="start" gap="md" className="m-bottom-sm">
-                        <input
-                          type="color"
-                          name={colorGroup}
-                          value={palette[colorGroup]}
-                          onChange={(event) => handleColorChange(colorGroup, event.target.value)}
-                        ></input>
-                        <Column>
-                          <label className="caption-bold mono m-bottom-2xs" htmlFor={colorGroup}>
-                            {colorGroup}
-                          </label>
-                          <p className="caption color-onsurfacevariant m-bottom-xs">
-                            {colorGroup === "primary"
-                              ? "Main brand color, used for most UI elements."
-                              : colorGroup === "secondary"
-                                ? "Desaturated variant of primary."
-                                : colorGroup === "tertiary"
-                                  ? "Your accent color. Defaults to complementary hue to primary."
+                    <h2 className="capline color-onsurfacevariant m-bottom-lg">Semantic</h2>
+                  </div>
+                  {semanticPalette.map((colorGroup) => (
+                    <Row key={colorGroup} alignItems="start" gap="md" className="m-bottom-sm">
+                      <input
+                        type="color"
+                        name={colorGroup}
+                        value={palette[colorGroup]}
+                        onChange={(event) => handleColorChange(colorGroup, event.target.value)}
+                      ></input>
+                      <Column>
+                        <label className="caption-bold mono m-bottom-2xs" htmlFor={colorGroup}>
+                          {colorGroup}
+                        </label>
+                        <p className="caption color-onsurfacevariant m-bottom-xs">
+                          {colorGroup === "error"
+                            ? "A pink or red, indicating problems."
+                            : colorGroup === "warning"
+                              ? "An orange or yellow, indicating caution."
+                              : colorGroup === "success"
+                                ? "A green, indicating success."
+                                : colorGroup === "info"
+                                  ? "A blue, indicating neutral information."
                                   : null}
-                          </p>
-                        </Column>
-                      </Row>
-                    ))}
-                  </div>
+                        </p>
+                      </Column>
+                    </Row>
+                  ))}
+                </div>
+                <div>
                   <div>
-                    <div>
-                      <h2 className="capline color-onsurfacevariant m-bottom-lg">Semantic</h2>
-                    </div>
-                    {semanticPalette.map((colorGroup) => (
-                      <Row key={colorGroup} alignItems="start" gap="md" className="m-bottom-sm">
-                        <input
-                          type="color"
-                          name={colorGroup}
-                          value={palette[colorGroup]}
-                          onChange={(event) => handleColorChange(colorGroup, event.target.value)}
-                        ></input>
-                        <Column>
-                          <label className="caption-bold mono m-bottom-2xs" htmlFor={colorGroup}>
-                            {colorGroup}
-                          </label>
-                          <p className="caption color-onsurfacevariant m-bottom-xs">
-                            {colorGroup === "error"
-                              ? "A pink or red, indicating problems."
-                              : colorGroup === "warning"
-                                ? "An orange or yellow, indicating caution."
-                                : colorGroup === "success"
-                                  ? "A green, indicating success."
-                                  : colorGroup === "info"
-                                    ? "A blue, indicating neutral information."
-                                    : null}
-                          </p>
-                        </Column>
-                      </Row>
-                    ))}
+                    <h2 className="capline color-onsurfacevariant m-bottom-lg">Layout</h2>
                   </div>
-                  <div>
-                    <div>
-                      <h2 className="capline color-onsurfacevariant m-bottom-lg">Layout</h2>
-                    </div>
-                    {layoutPalette.map((colorGroup) => (
-                      <Row key={colorGroup} alignItems="start" gap="md" className="m-bottom-sm">
-                        <input
-                          type="color"
-                          name={colorGroup}
-                          value={palette[colorGroup]}
-                          onChange={(event) => handleColorChange(colorGroup, event.target.value)}
-                        ></input>
-                        <Column>
-                          <label className="caption-bold mono m-bottom-xs" htmlFor={colorGroup}>
-                            {colorGroup}
-                          </label>
-                          <p className="caption color-onsurfacevariant m-bottom-xs">
-                            {colorGroup === "neutral"
-                              ? "Backgrounds, surfaces, outlines, and default text color"
-                              : colorGroup === "neutralvariant"
-                                ? "Surface variant, outline variant, and text color variant"
-                                : null}
-                          </p>
-                        </Column>
-                      </Row>
-                    ))}
-                  </div>
-                </Column>
-              </Card>
-            </div>,
-            document.body
-          )}
+                  {layoutPalette.map((colorGroup) => (
+                    <Row key={colorGroup} alignItems="start" gap="md" className="m-bottom-sm">
+                      <input
+                        type="color"
+                        name={colorGroup}
+                        value={palette[colorGroup]}
+                        onChange={(event) => handleColorChange(colorGroup, event.target.value)}
+                      ></input>
+                      <Column>
+                        <label className="caption-bold mono m-bottom-xs" htmlFor={colorGroup}>
+                          {colorGroup}
+                        </label>
+                        <p className="caption color-onsurfacevariant m-bottom-xs">
+                          {colorGroup === "neutral"
+                            ? "Backgrounds, surfaces, outlines, and default text color"
+                            : colorGroup === "neutralvariant"
+                              ? "Surface variant, outline variant, and text color variant"
+                              : null}
+                        </p>
+                      </Column>
+                    </Row>
+                  ))}
+                </div>
+              </Column>
+            </Card>
+          </div>,
+          document.body
+        )}
 
-        <style jsx>{`
-          input[type="color"] {
-            -webkit-appearance: none;
-            -moz-appearance: none;
-            appearance: none;
-            width: var(--lk-size-lg);
-            height: var(--lk-size-lg);
-            flex-basis: auto;
-            flex: 0 0 auto;
-            background-color: transparent;
-            border: none;
-            cursor: pointer;
-            outline: 2px solid var(--lk-onsurface);
+      <style jsx>{`
+        input[type="color"] {
+          -webkit-appearance: none;
+          -moz-appearance: none;
+          appearance: none;
+          width: var(--lk-size-lg);
+          height: var(--lk-size-lg);
+          flex-basis: auto;
+          flex: 0 0 auto;
+          background-color: transparent;
+          border: none;
+          cursor: pointer;
+          outline: 2px solid var(--lk-onsurface);
+          border-radius: 100em;
+
+          padding-block: 0px;
+          padding-inline: 0px;
+
+          &::-webkit-color-swatch-wrapper {
+            padding: 0;
+          }
+
+          &::-webkit-color-swatch {
             border-radius: 100em;
-
-            padding-block: 0px;
-            padding-inline: 0px;
-
-            &::-webkit-color-swatch-wrapper {
-              padding: 0;
-            }
-
-            &::-webkit-color-swatch {
-              border-radius: 100em;
-              border: none;
-            }
-            &::-moz-color-swatch {
-              border-radius: 100%;
-              border: none;
-            }
+            border: none;
           }
-
-          .lk-theme-drawer {
-            position: fixed;
-            top: 0;
-            left: 0;
-            bottom: 0;
-            overflow: hidden;
-
-            width: calc(var(--lk-size-4xl) * var(--lk-wholestep));
-            z-index: 1000;
-            padding: var(--lk-size-md);
+          &::-moz-color-swatch {
+            border-radius: 100%;
+            border: none;
           }
-        `}</style>
-      </>
-    );
-  }
+        }
+
+        .lk-theme-drawer {
+          position: fixed;
+          top: 0;
+          left: 0;
+          bottom: 0;
+          overflow: hidden;
+
+          width: calc(var(--lk-size-4xl) * var(--lk-wholestep));
+          z-index: 1000;
+          padding: var(--lk-size-md);
+        }
+      `}</style>
+    </>
+  );
 }
