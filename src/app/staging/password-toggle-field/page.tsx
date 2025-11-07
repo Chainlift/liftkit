2a8b3b4f8c3c1066cd09126ca21056a042241236
Garrett Mack <garrett.mack@morganlewis.com>
2025-11-06 16:07:11 -0800

    style password-toggle-field

--- src/app/staging/password-toggle-field/page.tsx
+++ src/app/staging/password-toggle-field/page.tsx
@@ -1,12 +1,16 @@
 // app/password-toggle-field/page.tsx
 
+import Icon from "@/registry/nextjs/components/icon";
 import {
   PasswordToggleField,
   PasswordToggleFieldInput,
   PasswordToggleFieldToggle,
   PasswordToggleFieldSlot,
   PasswordToggleFieldIcon,
-} from "@/registry/nextjs/components/password-toggle-field"
+} from "@/registry/nextjs/components/password-toggle-field";
+import TextInput from "@/registry/nextjs/components/text-input";
+import "./styles.css";
+import StateLayer from "@/registry/nextjs/components/state-layer";
 
 export default function Page() {
   return (
@@ -14,13 +18,14 @@ export default function Page() {
       <h1>Password Toggle Field Example</h1>
 
       <PasswordToggleField>
-        <PasswordToggleFieldInput placeholder="Enter password" />
-        <PasswordToggleFieldSlot>
+        <div className="password-toggle-field-wrapper">
+          <PasswordToggleFieldInput placeholder="Password" />
           <PasswordToggleFieldToggle>
-            <PasswordToggleFieldIcon />
+            <PasswordToggleFieldIcon visible={<Icon name="eye" />} hidden={<Icon name="eye-closed" />} />
+            <StateLayer />
           </PasswordToggleFieldToggle>
-        </PasswordToggleFieldSlot>
+        </div>
       </PasswordToggleField>
     </main>
-  )
+  );
 }
