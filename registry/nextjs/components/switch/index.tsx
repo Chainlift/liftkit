fba60ca1dc03408d0f0797d76a14c065c45b8874
Garrett Mack <garrett.mack@morganlewis.com>
2025-11-08 11:01:00 -0800

    finish styling switch

--- registry/nextjs/components/switch/index.tsx
+++ registry/nextjs/components/switch/index.tsx
@@ -1,37 +1,36 @@
 // switch.tsx
-"use client"
+"use client";
 
-import * as React from "react"
-import * as SwitchPrimitive from "@radix-ui/react-switch"
-import { cn } from "@/registry/nextjs/lib/utilities"
-import "./switch.css"
+import * as React from "react";
+import * as SwitchPrimitive from "@radix-ui/react-switch";
+import { cn } from "@/registry/nextjs/lib/utilities";
+import "./switch.css";
 
-const Switch = React.forwardRef<
+const SwitchRoot = React.forwardRef<
   React.ComponentRef<typeof SwitchPrimitive.Root>,
   React.ComponentProps<typeof SwitchPrimitive.Root>
 >(function Switch({ className, ...props }, ref) {
-  return (
-    <SwitchPrimitive.Root
-      ref={ref}
-      data-slot="switch-root"
-      className={cn("placeholder", className)}
-      {...props}
-    />
-  )
-})
+  return <SwitchPrimitive.Root ref={ref} data-slot="switch-root" className={cn("placeholder", className)} {...props} />;
+});
 
 const SwitchThumb = React.forwardRef<
   React.ComponentRef<typeof SwitchPrimitive.Thumb>,
   React.ComponentProps<typeof SwitchPrimitive.Thumb>
 >(function SwitchThumb({ className, ...props }, ref) {
   return (
-    <SwitchPrimitive.Thumb
-      ref={ref}
-      data-slot="switch-thumb"
-      className={cn("placeholder", className)}
-      {...props}
-    />
-  )
-})
+    <SwitchPrimitive.Thumb ref={ref} data-slot="switch-thumb" className={cn("placeholder", className)} {...props} />
+  );
+});
 
-export { Switch, SwitchThumb }
+const Switch = React.forwardRef<
+  React.ComponentRef<typeof SwitchPrimitive.Root>,
+  React.ComponentProps<typeof SwitchPrimitive.Root>
+>(function Switch({ className, ...props }, ref) {
+  return (
+    <SwitchRoot ref={ref} data-slot="switch-root" className={cn("placeholder", className)} {...props}>
+      <SwitchThumb />
+    </SwitchRoot>
+  );
+});
+
+export { Switch, SwitchThumb };
